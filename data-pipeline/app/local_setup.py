7597aa8588205ba8977570a0b90367e7c4edb86d
Wade Barnes <wade.barnes@shaw.ca>
2018-07-26 15:29:47 -0700

    #3 - Add support for a password on the mara-db

--- data-pipeline/app/local_setup.py
+++ data-pipeline/app/local_setup.py
@@ -14,8 +14,10 @@ def databases():
     mara_host = os.environ.get('MARA_DB_HOST', 'bcregdb')
     mara_database = os.environ.get('MARA_DB_DATABASE', 'mara_db')
     mara_user = os.environ.get('MARA_DB_USER', 'mara_db')
+    mara_password = os.environ.get('MARA_DB_PASSWORD')
+
     return {
-        'mara': mara_db.dbs.PostgreSQLDB(user=mara_user, host=mara_host, database=mara_database)
+        'mara': mara_db.dbs.PostgreSQLDB(user=mara_user, password=mara_password, host=mara_host, database=mara_database)
     }
 
 
