964635874f18a0e334d0430befe85f39508bd306
Ian Costanzo <ian@anon-solutions.ca>
2018-08-22 16:39:52 -0700

    Close sessions

--- data-pipeline/bcreg/submit-creds.py
+++ data-pipeline/bcreg/submit-creds.py
@@ -218,6 +218,7 @@ async def process_credential_queue():
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
+        http_client.close()
         if cur is not None:
             cur.close()
         if conn is not None:
