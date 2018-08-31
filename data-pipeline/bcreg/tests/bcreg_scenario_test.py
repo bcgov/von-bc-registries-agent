92ae714976982c7c78d66543bb2aea567f0edd67
Ian Costanzo <ian@anon-solutions.ca>
2018-08-31 11:52:10 -0700

    Updated scenario-based unit tests

--- data-pipeline/bcreg/tests/bcreg_scenario_test.py
+++ data-pipeline/bcreg/tests/bcreg_scenario_test.py
@@ -1,9 +1,18 @@
 
 import time
+import datetime
+import json
 
 from bcreg.bcregistries import BCRegistries, system_type
 from bcreg.eventprocessor import EventProcessor
 from bcreg.tests.sample_corps import sample_test_corps
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
 
 
 # basic BC corporation with an address and no DBA's
@@ -15,7 +24,7 @@ def test_scenario_basic_bc_corp():
     # TODO run some assertions based on what we expect
     print("# basic BC corporation with an address and no DBA's")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 2
     
 
@@ -28,7 +37,7 @@ def test_scenario_basic_dba_firm():
     # TODO run some assertions based on what we expect
     print("# basic BC corporation with an address and no DBA's")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 2
     
 
@@ -41,7 +50,7 @@ def test_scenario_basic_xcorp():
     # TODO run some assertions based on what we expect
     print("# basic ex-corp with a non-BC jurisdiction and an address")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 2
     
 
@@ -54,7 +63,7 @@ def test_scenario_assumed_name():
     # TODO run some assertions based on what we expect
     print("# basic ex-corp with an assumed name")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 2
     
 
@@ -67,7 +76,7 @@ def test_scenario_trans_name():
     # TODO run some assertions based on what we expect
     print("# basic BC corp with a translated name")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 2
     
 
@@ -80,8 +89,15 @@ def test_scenario_single_dba():
     # TODO run some assertions based on what we expect
     print("# basic corp with 1 DBA (no DBA address)")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 3
+
+    my_dba_num = 'FM3035075'
+    my_corp_dict['corp_num'] = my_dba_num
+    dba_creds = generate_creds_for_corp(my_corp_dict)
+    print("Corp: " + my_dba_num + " generated " + str(len(dba_creds)) + " credentials")
+    print(json.dumps(dba_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
+    assert len(dba_creds) == 1
     
 
 # basic corp with 1 DBA with an address
@@ -93,8 +109,15 @@ def test_scenario_dba_with_address():
     # TODO run some assertions based on what we expect
     print("# basic corp with 1 DBA with an address")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 3
+
+    my_dba_num = 'FM9129945'
+    my_corp_dict['corp_num'] = my_dba_num
+    dba_creds = generate_creds_for_corp(my_corp_dict)
+    print("Corp: " + my_dba_num + " generated " + str(len(dba_creds)) + " credentials")
+    print(json.dumps(dba_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
+    assert len(dba_creds) == 2
     
 
 # basic corp with multiple DBA's (3)
@@ -106,8 +129,19 @@ def test_scenario_multi_dbas():
     # TODO run some assertions based on what we expect
     print("# basic corp with multiple DBA's (3)")
     print("Corp: " + my_corp_num + " generated " + str(len(my_creds)) + " credentials")
-    print(my_creds)
+    print(json.dumps(my_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
     assert len(my_creds) == 5
+
+    my_dba_nums = ['FM3834099','FM8823648','FM9877026']
+    my_dba_ct = [1,1,2]
+    i = 0
+    for my_dba_num in my_dba_nums:
+        my_corp_dict['corp_num'] = my_dba_num
+        dba_creds = generate_creds_for_corp(my_corp_dict)
+        print("Corp: " + my_dba_num + " generated " + str(len(dba_creds)) + " credentials")
+        print(json.dumps(dba_creds, cls=DateTimeEncoder, indent=4, sort_keys=True))
+        assert len(dba_creds) == my_dba_ct[i]
+        i = i + 1
 
 
 # utility method to process the selected corp and generate credentails
