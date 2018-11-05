c14bde8b5f0202856b27c29c87fef5ef0cf8f88b
Ian Costanzo <ian@anon-solutions.ca>
2018-11-05 11:53:52 -0800

    Future dated credentials w.i.p.

--- data-pipeline/bcreg/insert.py
+++ data-pipeline/bcreg/insert.py
@@ -8,12 +8,12 @@ from bcreg.eventprocessor import EventProcessor
 from bcreg.eventprocessor import corp_credential, corp_schema, corp_version
 from bcreg.eventprocessor import addr_credential, addr_schema, addr_version
 from bcreg.eventprocessor import dba_credential, dba_schema, dba_version
-from bcreg.bcregistries import system_type
+from bcreg.bcregistries import system_type, MIN_START_DATE
 
 
 with EventProcessor() as event_processor:
     # insert last event
-    event_processor.insert_last_event(system_type, 0)
+    event_processor.insert_last_event(system_type, 0, MIN_START_DATE)
     
     print("Seeded initial event processor data")
 
