e5941082205433c068af7fe763d50fe4c6a73ba3
Wade Barnes <wade.barnes@shaw.ca>
2019-12-19 15:18:12 -0800

    Incorporate issuer.tob_connection_synced() into the health_check endpoint.
    - Also adjust startup times of containers in docker.

--- bcreg-aca/src/app.py
+++ bcreg-aca/src/app.py
@@ -24,7 +24,10 @@ wsgi_app = app.wsgi_app
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    return make_response(jsonify({'success': True}), 200)
+    if issuer.tob_connection_synced():
+        return make_response(jsonify({'success': True}), 200)
+    else:
+        abort(503, "Connection not yet synced")
 
 @app.route('/status/reset', methods=['GET'])
 def clear_status():
