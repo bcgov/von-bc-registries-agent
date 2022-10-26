c8e1f51e2b1b7fa625e81324e0fffb8375a305d5
Ian Costanzo <ian@anon-solutions.ca>
2022-10-26 10:57:09 -0700

    Update status script to report on both sources
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- data-pipeline/bcreg/display_pipeline_status.py
+++ data-pipeline/bcreg/display_pipeline_status.py
@@ -3,11 +3,15 @@ import psycopg2
 import datetime
 import json
 import decimal
+
+from bcreg.bcregistries import system_type
+from bcreg.bcreg_lear import lear_system_type
 from bcreg.config import config
 from bcreg.eventprocessor import EventProcessor
 
 
 with EventProcessor() as event_processor:
-    event_processor.display_event_processing_status()
+    event_processor.display_event_processing_status(system_type_cd=system_type)
 
+    event_processor.display_event_processing_status(system_type_cd=lear_system_type)
 
