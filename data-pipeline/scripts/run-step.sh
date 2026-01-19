e60dc1b4bbcf35681f293b5085008fe395f908d8
Wade Barnes <wade@neoterictech.ca>
2026-01-19 07:56:32 -0800

    Update run-step.sh to use a lock file.
    - Dynamically generate a lock file based on the pipeline being run.
      - This ensures different pipelines don't block each other, just themselves.
    - Exist if an instance of the pipeline is already running.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- data-pipeline/scripts/run-step.sh
+++ data-pipeline/scripts/run-step.sh
@@ -1,3 +1,18 @@
-# PYTHONPATH=../:. python3 "$@"
-cd .. && PYTHONPATH=. python3 "$@" && cd scripts
+#!/bin/bash
+pushd .. >/dev/null
 
+# Use flock to get a non-blocking lock on a lockfile while a given pipeline is running
+# to avoid multiple instances of the same pipeline from running.
+# The lock files are purposely NOT deleted to avoid possible race conditions.
+lockFileName=./$(basename "${@%%.*}").lock
+(
+    if ! flock -n 200; then
+        echo "Another instance of ${@} is already running. Exiting." >&2
+        exit 1
+    fi
+
+    # Run the requested pipeline
+    PYTHONPATH=. python3 "$@"
+) 200>|"${lockFileName}"
+
+popd >/dev/null
